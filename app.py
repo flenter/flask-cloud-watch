84873b0a4d265078e30865cea79bca312b67c9f0
Jacco Flenter <github@putpixel.nl>
2013-12-17 15:17:46 +0100

    form added

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import redis
 
 from flask import Flask
-from flask import render_template
+from flask import redirect, render_template
 
 app = Flask(__name__)
 app.redis = redis.StrictRedis(
@@ -10,6 +10,28 @@ app.redis = redis.StrictRedis(
     port=6379,
     db=0
 )
+app.debug = True
+app.secret_key = "$=q=p__8@-)pwvZl2nw!&c{wVn|0*6!:8@n(92_@r(9lkbbeb2"
+app.config.static = os.environ.get("STATIC_URL", "/static")
+
+from flask_wtf import Form
+from wtforms import TextField
+from wtforms.validators import Required
+
+
+class CloudForm(Form):
+    classification = TextField('classification', validators=[Required()])
+
+
+@app.route("/add", methods=['GET', 'POST'])
+def add_cloud():
+
+    form = CloudForm(csrf_enabled=False)
+
+    if form.validate_on_submit():
+        app.redis.rpush('clouds', form.classification.data)
+        return redirect('/')
+    return render_template('submit.html', form=form)
 
 
 @app.route("/")
@@ -19,5 +41,4 @@ def clouds():
 
 if __name__ == "__main__":
     port = int(os.getenv('PORT', 5000))
-    app.config.static = os.environ.get("STATIC_URL", "/static")
     app.run(host='0.0.0.0', port=port)
--- app_test.py
+++ app_test.py
@@ -1,15 +1,18 @@
 from app import app
 
+app.config['WTF_CSRF_ENABLED'] = False
+
 import unittest
 
 
-class CloudTestCase(unittest.TestCase):
+class CloudTestAppCase(unittest.TestCase):
 
     def setUp(self):
         app.redis.rpush('clouds', 'Altocumulus')
         app.redis.rpush('clouds', 'Altostratus')
         app.redis.rpush('clouds', 'Cumulonimbus')
         app.redis.rpush('clouds', 'Nimbostratus')
+        # app.debug = True
 
     def tearDown(self):
         app.redis.flushdb()
@@ -32,6 +35,43 @@ html")
         self.assertIn(
             'Nimbostratus', response.data, "Nimbostratus was not found in the \
 html")
+
+
+class CloudTestAddCloudCase(unittest.TestCase):
+
+    def tearDown(self):
+        app.redis.flushdb()
+
+    def test_add_cloud(self):
+        # print dir(app.config)
+        tester = app.test_client(self)
+        tester.WTF_CSRF_ENABLED = False
+
+        response = tester.get('/add', content_type='application/html')
+
+        assert 'This field is required.' not in response.data
+        assert 'New cloud' in response.data
+
+        response = tester.post(
+            '/add',
+            content_type='application/html',
+            data=dict()
+        )
+
+        assert 'This field is required.' in response.data
+        assert 'New cloud' in response.data
+
+        response = tester.post(
+            '/add',
+            data=dict(
+                classification="my_special_test_cloud"
+            ))
+
+        # print dir(response), response.status_code, dir(response.response)
+        db_result = app.redis.lrange("clouds", 0, -1)
+
+        assert "my_special_test_cloud" in db_result
+        assert "http://localhost/" == response.location
 
 if __name__ == '__main__':
     unittest.main()
